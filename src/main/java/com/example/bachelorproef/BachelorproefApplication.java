41e714dfab52fe3b6b93244808063ea4cbb66c9b
aaronpollet <aaron.pollet@student.hogent.be>
2024-05-22 07:25:14 +0200

    update to make a pull request

--- src/main/java/com/example/bachelorproef/BachelorproefApplication.java
+++ src/main/java/com/example/bachelorproef/BachelorproefApplication.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class BachelorproefApplication {
